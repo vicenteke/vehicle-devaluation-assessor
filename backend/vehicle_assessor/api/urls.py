478d0eea5c06dd91cf73be0282ea3669b6c84e1a
Vicente Knihs Erbs <vicenteknihs@gmail.com>
2024-04-07 14:20:14 -0300

    feat: implement vehicle assessment view

--- backend/vehicle_assessor/api/urls.py
+++ backend/vehicle_assessor/api/urls.py
@@ -4,11 +4,23 @@ from .views import (
     ConstantsBrandsView,
     ConstantsModelsView,
     ConstantsYearsView,
+    VehicleAssessorView,
 )
 
 urlpatterns = [
-    path('constants/<str:vehicleType>/<str:brandId>/<str:modelId>/years', ConstantsYearsView.as_view()),
-    path('constants/<str:vehicleType>/<str:brandId>/models', ConstantsModelsView.as_view()),
+    path(
+        'constants/<str:vehicleType>/<str:brandId>/<str:modelId>/years',
+        ConstantsYearsView.as_view()
+    ),
+    path(
+        'constants/<str:vehicleType>/<str:brandId>/models',
+        ConstantsModelsView.as_view()
+    ),
     path('constants/<str:vehicleType>/brands', ConstantsBrandsView.as_view()),
     path('constants/vehicle_types', ConstantsVehicleTypesView.as_view()),
+    path(
+        '<str:vehicleType>/<str:brandId>/<str:modelId>/<str:yearId>'
+        '/<int:acquisitionPrice>/<int:acquisitionYear>',
+        VehicleAssessorView.as_view()
+    ),
 ]
--- backend/vehicle_assessor/api/views.py
+++ backend/vehicle_assessor/api/views.py
@@ -1,7 +1,10 @@
 import requests
 import logging
-from rest_framework.views import APIView
+from datetime import datetime
+
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .enumerators import VehicleType
 
@@ -19,7 +22,7 @@ class ConstantsVehicleTypesView(APIView):
             {
                 "label": string,
                 "value": string
-            }
+            },
             ...
         ]
         """
@@ -41,12 +44,13 @@ class ConstantsBrandsView(APIView):
             {
                 "label": string,
                 "value": string
-            }
+            },
             ...
         ]
         """
         response = requests.get(
-            f'https://fipe.parallelum.com.br/api/v2/{kwargs.get("vehicleType")}/brands'
+            f'https://fipe.parallelum.com.br/api/v2/'
+            f'{kwargs.get("vehicleType")}/brands'
         )
         if response.status_code != 200:
             return Response(response.reason, response.status_code)
@@ -69,12 +73,14 @@ class ConstantsModelsView(APIView):
             {
                 "label": string,
                 "value": string
-            }
+            },
             ...
         ]
         """
         response = requests.get(
-            f'https://fipe.parallelum.com.br/api/v2/{kwargs.get("vehicleType")}/brands/{kwargs.get("brandId")}/models'
+            f'https://fipe.parallelum.com.br/api/v2/'
+            f'{kwargs.get("vehicleType")}/brands/{kwargs.get("brandId")}'
+            '/models'
         )
         if response.status_code != 200:
             return Response(response.reason, response.status_code)
@@ -97,12 +103,14 @@ class ConstantsYearsView(APIView):
             {
                 "label": string,
                 "value": string
-            }
+            },
             ...
         ]
         """
         response = requests.get(
-            f'https://fipe.parallelum.com.br/api/v2/{kwargs.get("vehicleType")}/brands/{kwargs.get("brandId")}/models/{kwargs.get("modelId")}/years'
+            f'https://fipe.parallelum.com.br/api/v2/'
+            f'{kwargs.get("vehicleType")}/brands/{kwargs.get("brandId")}'
+            f'/models/{kwargs.get("modelId")}/years'
         )
         if response.status_code != 200:
             return Response(response.reason, response.status_code)
@@ -116,4 +124,102 @@ class ConstantsYearsView(APIView):
 
 
 class VehicleAssessorView(APIView):
-    """Return assessment for vehicle"""
+    def get(self, request, format=None, **kwargs):
+        """
+        Return vehicle assesment
+
+        Response:
+        {
+            "current_price": float,             # current vehicle price in BRL
+            "original_price": float | null,     # original vehicle price in BRL
+            "variation": float,                 # vehicle price variation
+            "inflation": float                  # inflation on time interval
+        }
+        """
+        now = datetime.now()
+        vehicle_type = kwargs.get('vehicleType')
+        brand_id = kwargs.get('brandId')
+        model_id = kwargs.get('modelId')
+        year_id = kwargs.get('yearId')
+        acquisition_price = kwargs.get('acquisitionPrice')
+        acquisition_year = kwargs.get('acquisitionYear')
+        if acquisition_year > now.year or acquisition_year < 1920:
+            return Response(
+                'Acquisition year must be a number',
+                status.HTTP_400_BAD_REQUEST
+            )
+
+        # get current price
+        response = requests.get(
+            f'https://fipe.parallelum.com.br/api/v2/{vehicle_type}'
+            f'/brands/{brand_id}/models/{model_id}/years/{year_id}'
+        )
+        if response.status_code != 200:
+            return Response(response.reason, response.status_code)
+
+        response_price = response.json()['price']
+        current_price = float(response_price.replace('.', '')[3:-3])
+        variation = (current_price / acquisition_price) - 1
+
+        # get original price for acquisition date
+        original_price = None
+        reference_id = None
+        references = requests.get(
+            'https://fipe.parallelum.com.br/api/v2/references'
+        )
+        if references.status_code != 200:
+            logger.warning('Unable to get reference month for original price')
+            return Response(references.reason, references.status_code)
+        else:
+            references = references.json()
+            reference_id = None
+            for reference in references:
+                if str(acquisition_year) in reference['month']:
+                    reference_id = reference['code']
+                    break
+
+            if reference_id:
+                response = requests.get(
+                    f'https://fipe.parallelum.com.br/api/v2/{vehicle_type}'
+                    f'/brands/{brand_id}/models/{model_id}/years/{year_id}'
+                    f'?reference={reference_id}'
+                )
+                if response.status_code != 200:
+                    logger.warning('Unable to get vehicle original price')
+                else:
+                    response_price = response.json()['price']
+                    original_price = float(response_price.replace('.', '')[3:-3])
+
+        # get inflation
+        inflation = None
+        current_year = now.year
+        last_inflation_month = now.month - 2
+        if last_inflation_month < 1:
+            last_inflation_month = 12 + last_inflation_month - 1
+            current_year -= 1
+            acquisition_year -= 1
+
+        start_month_code = str(acquisition_year) +\
+            "{:02d}".format(last_inflation_month)
+        current_month_code = str(current_year) +\
+            "{:02d}".format(last_inflation_month)
+
+        response = requests.get(
+            f'https://servicodados.ibge.gov.br/api/v3/agregados/1737/periodos/'
+            f'{start_month_code}%7C{current_month_code}/variaveis/'
+            f'2266?localidades=N1[all]'
+        )
+        if response.status_code != 200:
+            logger.warning('Unable to get inflation')
+        else:
+            data = response.json()[0]['resultados'][0]['series'][0]['serie']
+            start_rate = float(data[start_month_code])
+            current_rate = float(data[current_month_code])
+            inflation = current_rate / start_rate
+
+        return Response({
+            'current_price': current_price,
+            'original_price': original_price,
+            'variation': variation,
+            'inflation': inflation
+        })
